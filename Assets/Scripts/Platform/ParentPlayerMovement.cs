f3e68e87af40ed6640a6b2e3c44d977643524b71
Markus Damm <markus.damm@hs-furtwangen.de>
2023-06-16 20:54:37 +0200

    Adjust room, add elevator-model, walls and cinery

--- Assets/Scripts/Platform/ParentPlayerMovement.cs
+++ Assets/Scripts/Platform/ParentPlayerMovement.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class ParentPlayerMovement : MonoBehaviour
 {
+    Transform playerTransform;
+    MovingPlatform platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platform = GetComponent<MovingPlatform>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("collision with " + collision);
         if (collision.gameObject.tag == "Player")
         {
-            collision.transform.parent.parent.SetParent(transform);
+            playerTransform = collision.transform.parent.parent;
+            playerTransform.SetParent(transform);
+            // collision.transform.parent.parent.SetParent(transform);
             // collision.transform.SetParent(transform);
         }
     }
@@ -16,8 +27,12 @@ public class ParentPlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            collision.transform.parent.parent.SetParent(null);
+            playerTransform.SetParent(null);
+            // playerTransform = null;
+            // collision.transform.parent.parent.SetParent(null);
             // collision.transform.SetParent(null);
+
+            platform.canMove = false;
         }
     }
 }
